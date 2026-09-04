1afba28b46932ca8681e6d9d5fb1bd0912bbe089
Gachi707 <ggachuiri6@gmail.com>
2026-09-04 12:48:20 +0300

    Add files via upload

---
+++ config.py.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "reflex_db")
+    DB_PORT = int(os.getenv("DB_PORT", "3306"))
